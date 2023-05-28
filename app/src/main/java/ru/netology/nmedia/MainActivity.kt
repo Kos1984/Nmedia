cb4cf78be9ada18b1cdd185f44d9a412afbd68c8
Kos1984 <86412949+Kos1984@users.noreply.github.com>
2023-05-28 11:46:05 +0300

    MVVM created

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,7 +2,9 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.viewModel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,38 +13,29 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val post = Post()
-
-        with(binding){
-            authorTextView.text = post.author
-            postDateView.text =  post.published
-            postContentView.text = post.content
-            likeCount.text = PostService.countToString(post.likesCount)
-            shareCount.text = PostService.countToString(post.shareCount)
-            viewsCount.text = PostService.countToString(post.viewsCount)
-
-
-
-            if (post.likedByMe){
-                likeImageView.setImageResource(R.drawable.baseline_favorite_red_24)
-            }
-
-            likeImageView.setOnClickListener{
-                post.likedByMe = !post.likedByMe
-                post.likesCount = if (post.likedByMe) post.likesCount +1 else post.likesCount -1
+        val viewModel: PostViewModel  by viewModels()
+        viewModel.data.observe(this) { post ->
+            with(binding) {
+                authorTextView.text = post.author
+                postDateView.text = post.published
+                postContentView.text = post.content
                 likeCount.text = PostService.countToString(post.likesCount)
-                likeImageView.setImageResource(if (post.likedByMe){
-                    R.drawable.baseline_favorite_red_24
-                } else R.drawable.baseline_favorite_border_24)
-            }
-
-            shareImageView.setOnClickListener {
-                post.shareCount++
+                shareCount.text = PostService.countToString(post.shareCount)
+                viewsCount.text = PostService.countToString(post.viewsCount)
+                if (post.likedByMe){
+                    likeImageView.setImageResource(R.drawable.baseline_favorite_red_24)
+                } else likeImageView.setImageResource(R.drawable.baseline_favorite_border_24)
+                likeCount.text = PostService.countToString(post.likesCount)
                 shareCount.text = PostService.countToString(post.shareCount)
             }
-
         }
-
-
+        binding.likeImageView.setOnClickListener {
+            viewModel.like()
+        }
+        binding.shareImageView.setOnClickListener {
+            viewModel.share()
+        }
     }
+
+
 }
--- app/src/main/java/ru/netology/nmedia/PostService.kt
+++ app/src/main/java/ru/netology/nmedia/PostService.kt
@@ -4,7 +4,7 @@ object PostService{
 
     fun countToString (count: Long) : String {
         val result = when (count) {
-            in 1..999 ->   count.toString()
+            in 0..999 ->   count.toString()
             in 1000 .. 9_999 -> "%.1f".format(count / 1000.0) + "K"
             in 10_000 .. 999_999-> "%.0f".format(count / 1000.0) + "K"
             in 1_000_000 .. Long.MAX_VALUE -> "%.1f".format(count.toDouble()/ 1_000_000.0)+ "M"
