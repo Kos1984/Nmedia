1badf3977c3af98cf475c4fc8f32213fb6e6fe35
Kos1984 <nemcevk@gmail.com>
2023-06-01 12:46:36 +0300

    recycle viev

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -3,6 +3,7 @@ package ru.netology.nmedia
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import ru.netology.nmedia.adapter.PostAdapter
 import ru.netology.nmedia.databinding.ActivityMainBinding
 import ru.netology.nmedia.viewModel.PostViewModel
 
@@ -10,32 +11,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        val activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
 
         val viewModel: PostViewModel  by viewModels()
-        viewModel.data.observe(this) { post ->
-            with(binding) {
-                authorTextView.text = post.author
-                postDateView.text = post.published
-                postContentView.text = post.content
-                likeCount.text = PostService.countToString(post.likesCount)
-                shareCount.text = PostService.countToString(post.shareCount)
-                viewsCount.text = PostService.countToString(post.viewsCount)
-                if (post.likedByMe){
-                    likeImageView.setImageResource(R.drawable.baseline_favorite_red_24)
-                } else likeImageView.setImageResource(R.drawable.baseline_favorite_border_24)
-                likeCount.text = PostService.countToString(post.likesCount)
-                shareCount.text = PostService.countToString(post.shareCount)
-            }
+        val adapter = PostAdapter({viewModel.likeById(it.id )}, {viewModel.shareById(it.id)})
+
+
+        viewModel.data.observe(this) { posts ->
+            adapter.submitList(posts)
         }
-        binding.likeImageView.setOnClickListener {
-            viewModel.like()
-        }
-        binding.shareImageView.setOnClickListener {
-            viewModel.share()
-        }
+
+        activityMainBinding.list.adapter = adapter
+
     }
-
-
 }
+
