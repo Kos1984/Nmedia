a03085ebb294346e521110e64e2538e9b3d1549a
Kos1984 <nemcevk@gmail.com>
2023-06-16 13:09:06 +0300

    material desing change image to button

--- app/src/main/java/ru/netology/nmedia/adapter/PostViewHolder.kt
+++ app/src/main/java/ru/netology/nmedia/adapter/PostViewHolder.kt
@@ -18,14 +18,10 @@ class PostViewHolder(
             authorTextView.text = post.author
             postDateView.text = post.published
             postContentView.text = post.content
-            likeCount.text = PostService.countToString(post.likesCount)
-            shareCount.text = PostService.countToString(post.shareCount)
             viewsCount.text = PostService.countToString(post.viewsCount)
-            if (post.likedByMe){
-                likeImageView.setImageResource(R.drawable.baseline_favorite_red_24)
-            } else likeImageView.setImageResource(R.drawable.baseline_favorite_border_24)
-            likeCount.text = PostService.countToString(post.likesCount)
-            shareCount.text = PostService.countToString(post.shareCount)
+            likeImageView.isChecked = post.likedByMe
+            likeImageView.text = PostService.countToString(post.likesCount)
+            shareImageView.text = PostService.countToString(post.shareCount)
 
             likeImageView.setOnClickListener {
                 listener.onLike(post)
